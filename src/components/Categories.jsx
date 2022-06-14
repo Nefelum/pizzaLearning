e423bfd5dcb63b3fa7207967cf1b05879af0b3cb
dmitriy <adimas1672@gmail.com>
2022-06-14 14:10:43 +0400

    #4

---
+++ src/components/Categories.jsx
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+
+function Categories() {
+
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const categories = [
+        'Все',
+        'Мясные',
+        'Вегетарианская',
+        'Гриль',
+        'Острые',
+        'Закрытые',
+    ]
+
+    const onClickCategory = (index) => {
+        setActiveIndex(index)
+    };
+
+    return (
+        <div className="categories">
+            <ul>
+                {
+                  categories.map((pizzaCategory, i) => {
+                      return (<>
+                          <li onClick={() => onClickCategory(i)} className={activeIndex === i ? "active" : ""}>{pizzaCategory}
+                          </li>
+                        </>
+                      )
+                  })
+                }
+            </ul>
+        </div>
+    );
+}
+
+export default Categories;

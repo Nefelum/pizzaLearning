7a10b4c027de1802ecccb3106020fb103a91e7b3
dmitriy <adimas1672@gmail.com>
2022-06-20 10:57:03 +0400

    №10 props drilling

--- src/App.js
+++ src/App.js
@@ -9,13 +9,16 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Cart from "./pages/Cart"
+import {useState} from "react";
 
 
 function App() {
+    const [searchValue, setSearchValue] = useState('')
+    console.log(searchValue)
 
     return (
       <div className="wrapper">
-        <Header />
+        <Header searchValue={searchValue} setSearchValue={setSearchValue}/>
         <div className="content">
               <Routes>
                   <Route path="/" element={<Home />} />
